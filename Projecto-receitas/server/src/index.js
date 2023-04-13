947c50b24fdc0c735372632b0617898c9f8220a8
RicJM <116638687+Rimcant@users.noreply.github.com>
2023-04-13 10:06:08 +0100

    13-04-23

--- Projecto-receitas/server/src/index.js
+++ Projecto-receitas/server/src/index.js
@@ -1,17 +1,27 @@
 import express from "express";
-import cors from 'cors'
-import mongoose from 'mongoose'
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-import { userRouter } from './routes/users.js'
+import { userRouter } from './routes/users.js';
 import { recipesRouter } from "./routes/recipes.js";
 
-const app = express()
+const app = express();
 
+// Middleware para permitir o uso do formato JSON nas requisições
 app.use(express.json());
+
+// Middleware para permitir o acesso à API de outros domínios (CORS)
 app.use(cors());
+
+// Middleware para as rotas do endpoint de autenticação
 app.use("/auth", userRouter);
-app.use("/recipes")
 
-mongoose.connect("mongodb+srv://receitasapp:FPEZnjRImn5diruo@recipes.nz4fv7v.mongodb.net/recipes?retryWrites=true&w=majority");
+// Middleware para as rotas do endpoint de receitas
+app.use("/recipes", recipesRouter);
 
-app.listen(3001, () => console.log("SERVER STARTED"))
+// Conexão com o MongoDB Atlas
+mongoose.connect("mongodb+srv://<username>:<password>@<cluster-url>/recipes?retryWrites=true&w=majority");
+
+// Inicia o servidor na porta 3001
+app.listen(3001, () => console.log("Servidor iniciado na porta 3001"));
+
--- Projecto-receitas/server/src/routes/recipes.js
+++ Projecto-receitas/server/src/routes/recipes.js
@@ -5,6 +5,7 @@ import { UserModel } from '../models/Users.js';
 
 const router = express.Router();
 
+// Rota para buscar todas as receitas cadastradas
 router.get('/', async (req, res) => {
   try {
     const recipes = await RecipeModel.find({});
@@ -14,13 +15,31 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Rota para criar uma nova receita
 router.post('/', async (req, res) => {
   const { name, ingredients, instructions, imageURL, cookingTime, userOwner } = req.body;
 
-  if (!name || !ingredients || !instructions || !imageURL || !cookingTime || !userOwner) {
-    return res.status(400).json({ message: 'Missing required fields' });
+  // Verifica se todos os campos obrigatórios foram preenchidos
+  if (!name) {
+    return res.status(400).json({ message: 'Missing required field: name' });
+  }
+  if (!ingredients) {
+    return res.status(400).json({ message: 'Missing required field: ingredients' });
+  }
+  if (!instructions) {
+    return res.status(400).json({ message: 'Missing required field: instructions' });
+  }
+  if (!imageURL) {
+    return res.status(400).json({ message: 'Missing required field: imageURL' });
+  }
+  if (!cookingTime) {
+    return res.status(400).json({ message: 'Missing required field: cookingTime' });
+  }
+  if (!userOwner) {
+    return res.status(400).json({ message: 'Missing required field: userOwner' });
   }
 
+  // Cria a nova receita a partir dos dados enviados na requisição
   const recipe = new RecipeModel({
     name,
     ingredients,
@@ -31,6 +50,7 @@ router.post('/', async (req, res) => {
   });
 
   try {
+    // Salva a receita no banco de dados e retorna os dados da receita salva
     const savedRecipe = await recipe.save();
     res.json(savedRecipe);
   } catch (error) {
@@ -38,11 +58,13 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Rota para atualizar uma receita existente
 router.put("/:recipeId", async (req, res) => {
   const { name, ingredients, instructions, imageURL, cookingTime } = req.body;
   const { recipeId } = req.params;
 
   try {
+    // Busca a receita pelo ID e atualiza os dados
     const updatedRecipe = await RecipeModel.findByIdAndUpdate(recipeId, {
       name,
       ingredients,
@@ -57,5 +79,29 @@ router.put("/:recipeId", async (req, res) => {
   }
 });
 
+// Rota para buscar os IDs das receitas salvas pelo usuário
+router.get("/savedRecipes/ids", async (req, res) => {
+  try {
+    // Busca o usuário pelo ID e retorna os IDs das receitas salvas
+    const user = await UserModel.findById(req.query.userId)
+    res.json({savedRecipes: user?.savedRecipes})
+  } catch (error) {
+    res.json(error)
+  }
+})
+// Rota para buscar as receitas salvas pelo usuário
+router.get("/savedRecipes", async (req, res) => {
+  try {
+    // Busca o usuário pelo ID e busca todas as receitas que possuem ID salvo na lista de receitas salvas do usuário
+    const user = await UserModel.findById(req.query.userId)
+    const savedRecipes = await RecipeModel.find({
+      _id: { $in: user.savedRecipes}
+    })
+    res.json({savedRecipes})
+  } catch (error) {
+    res.json(error)
+  }
+})
 
 export { router as recipesRouter };
+
